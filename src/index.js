bb68c46693fa306243f30fd0e8c99a40a2c26461
chaojunyang <chaojunyang@yunify.com>
2022-06-07 08:35:37 +0800

    chore: 默认初始配置

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { React, ReactDOM } from "./CONST";
+import App from "./App";
+import "./index.css";
+// import jsx from "./pages/ExamplePage";
+import DiffPage from "./pages/DiffPage";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// console.log("version-sy-log", React.version); //sy-log
