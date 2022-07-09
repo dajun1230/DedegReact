fa80d3dfac00c5a44b26470b4f6434edacf5ffb5
杨朝军 <ycj01351761@alibaba-inc.com>
2022-07-09 11:18:08 +0800

    feat: 初始化app文件

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { React, ReactDOM } from "./CONST";
 import App from "./App";
+import DefaultApp from './defaultApp';
 import "./index.css";
 // import jsx from "./pages/ExamplePage";
 import DiffPage from "./pages/DiffPage";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<DefaultApp />, document.getElementById("root"));
 
 // console.log("version-sy-log", React.version); //sy-log
